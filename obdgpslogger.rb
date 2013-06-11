3c8df2b4906cb4791d0f3e27285f81daa562782a
Toshiki TAKEUCHI <take@xcoo.jp>
2013-06-12 02:36:18 +0900

    Modify license to BSD 2 Clause

--- obdgpslogger.rb
+++ obdgpslogger.rb
@@ -1,25 +1,6 @@
-#
-#   homebrew-obdgpslogger
-#   http://github.com/totakke/homebrew-obdgpslogger
-#   Copyright (C) 2013, Toshiki TAKEUCHI.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
 require 'formula'
 
 class Obdgpslogger < Formula
-
   homepage 'http://icculus.org/obdgpslogger/'
   url 'http://icculus.org/obdgpslogger/downloads/obdgpslogger-0.16.tar.gz'
   sha1 'a47c79b3c0570ed831f713ebf8c370c6e64b7a6c'
@@ -42,5 +23,4 @@ class Obdgpslogger < Formula
     system 'make'
     system 'make install'
   end
-
 end
